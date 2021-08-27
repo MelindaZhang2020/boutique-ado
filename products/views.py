ecbe529092a58d4c032f37577b6ad7630f9b5afa
Melinda Zhang <melinda_hongye@yahoo.com>
2021-08-27 13:07:35 +0100

    Add sorting tags,sort selector and results display

--- products/views.py
+++ products/views.py
@@ -20,7 +20,9 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=lower('name'))
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
